cae1640db88be358da9bdfaedc26cf4aaaaae69d
Gavin Ying <yying8751@yahoo.com>
2014-08-07 10:56:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TmRor2::Application.config.secret_token = '763edcccedeb400afaa13d9d1201c1e28ea490cf122771f04b33a48266736ea8ebc238fe05ac7bae138a636e57a2caa4e45d300a8074fc89f14e74daa7a16fec'
